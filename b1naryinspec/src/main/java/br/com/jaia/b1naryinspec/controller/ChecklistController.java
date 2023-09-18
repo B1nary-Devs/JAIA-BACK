6efac3eebda691c52182ee957507f45773a599b6
Marcelo Silva <marcelo.silva070899@gmail.com>
2023-09-18 19:20:22 -0300

    40- Criar Classe Checklist

--- b1naryinspec/src/main/java/br/com/jaia/b1naryinspec/controller/ChecklistController.java
+++ b1naryinspec/src/main/java/br/com/jaia/b1naryinspec/controller/ChecklistController.java
@@ -2,12 +2,12 @@ package br.com.jaia.b1naryinspec.controller;
 
 
 import br.com.jaia.b1naryinspec.service.ChecklistService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import br.com.jaia.b1naryinspec.model.Checklist;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/checklist")
@@ -20,9 +20,54 @@ public class ChecklistController {
         this.checklistService = checklistService;
     }
 
+
     @GetMapping
-    public List<Checklist> findAll(){
-        return checklistService.findAll();
+    public ResponseEntity<List<Checklist>> buscarTudo(){
+            return ResponseEntity.status(HttpStatus.OK).body(checklistService.buscarTudo());
     }
 
+    @PostMapping
+    public ResponseEntity<Object> novoChecklist(@RequestBody Checklist checklist) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(checklistService.salvar(checklist));
+    }
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> buscarPorId(@PathVariable("id") Long id) {
+        Checklist checklistOptional = checklistService.findByChecklistId(id);
+        if (checklistOptional == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Checklist não encontrado!");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(checklistOptional);
+    }
+
+    @GetMapping(value = "/nome/{nome}")
+    public ResponseEntity<Object> buscarPorNome(@PathVariable("nome") String nome) {
+        Checklist checklistOptional = checklistService.findByChecklistNome(nome);
+        if (checklistOptional == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Checklist não encontrado!");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(checklistOptional);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateChecklist(@PathVariable(value = "id") Long id,
+                                                  @RequestBody Checklist checklist){
+        Checklist checklistOptional = checklistService.findByChecklistId(id);
+        if (checklistOptional == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado!");
+        }
+        checklist.setChecklistId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(checklistService.salvar(checklist));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteChecklist(@PathVariable(value = "id") Long id){
+        Checklist checklistOptional = checklistService.findByChecklistId(id);
+        if (checklistOptional == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado!");
+        }
+        checklistService.delete(checklistOptional);
+        return ResponseEntity.status(HttpStatus.OK).body("Checklist Deletado");
+    }
 }
--- b1naryinspec/src/main/java/br/com/jaia/b1naryinspec/model/Checklist.java
+++ b1naryinspec/src/main/java/br/com/jaia/b1naryinspec/model/Checklist.java
@@ -9,32 +9,32 @@ public class Checklist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "checklist_id")
-    private Long checklist_id;
+    private Long checklistId;
 
-    @Column(name = "checklist_nome")
-    private String checklist_nome;
+    @Column(name = "checklist_nome", nullable = false)
+    private String checklistNome;
 
     public Checklist(Long checklist_id, String checklist_nome) {
-        this.checklist_id = checklist_id;
-        this.checklist_nome = checklist_nome;
+        this.checklistId = checklist_id;
+        this.checklistNome = checklist_nome;
     }
 
     public Checklist(){
     }
 
-    public Long getChecklist_id() {
-        return checklist_id;
+    public Long getChecklistId() {
+        return checklistId;
     }
 
-    public void setChecklist_id(Long checklist_id) {
-        this.checklist_id = checklist_id;
+    public void setChecklistId(Long checklistId) {
+        this.checklistId = checklistId;
     }
 
-    public String getChecklist_nome() {
-        return checklist_nome;
+    public String getChecklistNome() {
+        return checklistNome;
     }
 
-    public void setChecklist_nome(String checklist_nome) {
-        this.checklist_nome = checklist_nome;
+    public void setChecklistNome(String checklistNome) {
+        this.checklistNome = checklistNome;
     }
 }
