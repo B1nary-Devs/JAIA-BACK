4c0b836f35c410cdbd44138506c79a6ec19a5316
Marcelo Silva <marcelo.silva070899@gmail.com>
2023-09-18 20:10:35 -0300

    40- Criar Classe Checklist

--- b1naryinspec/src/main/java/br/com/jaia/b1naryinspec/model/Checklist.java
+++ b1naryinspec/src/main/java/br/com/jaia/b1naryinspec/model/Checklist.java
@@ -14,15 +14,14 @@ public class Checklist {
     @Column(name = "checklist_nome", nullable = false)
     private String checklistNome;
 
-    public Checklist(Long checklist_id, String checklist_nome) {
-        this.checklistId = checklist_id;
-        this.checklistNome = checklist_nome;
+    public Checklist(Long checklistId, String checklistNome) {
+        this.checklistId = checklistId;
+        this.checklistNome = checklistNome;
     }
 
     public Checklist(){
     }
 
-<<<<<<< HEAD
     public Long getChecklistId() {
         return checklistId;
     }
@@ -37,21 +36,5 @@ public class Checklist {
 
     public void setChecklistNome(String checklistNome) {
         this.checklistNome = checklistNome;
-=======
-    public Long getChecklist_id() {
-        return checklist_id;
-    }
-
-    public void setChecklist_id(Long checklist_id) {
-        this.checklist_id = checklist_id;
-    }
-
-    public String getChecklist_nome() {
-        return checklist_nome;
-    }
-
-    public void setChecklist_nome(String checklist_nome) {
-        this.checklist_nome = checklist_nome;
->>>>>>> 1Sprint
     }
 }
