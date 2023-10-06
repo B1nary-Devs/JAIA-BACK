caf64c55ea4c922f163c4d4ad260511843cc7ad4
GuiAlvesdev <guialves1353@gmail.com>
2023-10-05 21:25:35 -0300

    working in cors config

--- src/main/java/br/com/jaia/b1naryinspec/config/WebConfig.java
+++ src/main/java/br/com/jaia/b1naryinspec/config/WebConfig.java
@@ -13,6 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
     }
 }
