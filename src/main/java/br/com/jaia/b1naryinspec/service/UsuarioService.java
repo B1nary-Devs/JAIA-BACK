0064e12b6e906c89a36fed1c4e790f778d4ec188
Ludmila <ghansth@gmail.com>
2023-10-22 21:32:18 -0300

    99- Created usuario service

---
+++ src/main/java/br/com/jaia/b1naryinspec/service/UsuarioService.java
@@ -0,0 +1,46 @@
+package br.com.jaia.b1naryinspec.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.jaia.b1naryinspec.dto.UsuarioDTO;
+import br.com.jaia.b1naryinspec.model.Usuario;
+import br.com.jaia.b1naryinspec.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+public class UsuarioService {
+    
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+
+    @Transactional
+    public Usuario novoUsuario(UsuarioDTO usuarioDTO){
+        if(usuarioDTO == null){
+            throw new IllegalArgumentException("Dados inválidos!");
+        }
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail(usuarioDTO.getEmail());
+        usuario.setSenha(usuarioDTO.getSenha());
+
+        return usuarioRepo.save(usuario);
+    }
+
+    @Transactional
+    public List<Usuario> buscarTodosUsuarios(){
+        return usuarioRepo.findAll();
+    }
+
+    @Transactional
+    public Usuario buscarUsuarioId(Long usuarioId){
+        Optional<Usuario> usuarioOp = usuarioRepo.findById(usuarioId);
+        if(usuarioOp.isEmpty()){
+            throw new IllegalArgumentException("Usuário não encontrado!");
+        }
+        return usuarioOp.get();
+    }
+}
