f92dfcf34741aef04afb13575da1bf9d9ba1a63a
Ludmila <ghansth@gmail.com>
2023-11-13 00:21:10 -0300

    149- Solicitacao repository created

---
+++ src/main/java/br/com/jaia/b1naryinspec/repository/SolicitacaoRepository.java
@@ -0,0 +1,11 @@
+package br.com.jaia.b1naryinspec.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.jaia.b1naryinspec.model.Solicitacao;
+
+@Repository
+public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long> {
+    
+}
